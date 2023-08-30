1b8c350b88225ffbe1acfe8aa19c3b11558bd6a3
Utsav <utsavtaliyan544@gmail.com>
2023-08-30 23:22:15 +0530

    backend part 1 done

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
-const port1 = 3001;
-const bodyparser = require('body-parser');
 
-// writing an express server
+// creating an express server
 const app = express();
+
+// NODE PACKAGE MANAGER has all the external libraries such as express which you can install in your node environment 
+// and then use them easily
+
+// middleware1
 
 let numberOfRequests = 0;
 
-// function middleware1(req,res,next)
-// {
-//     numberOfRequests++;
-//     console.log(numberOfRequests);
-//     console.log('from inside the middleware '+req.headers.val);
-//     next();
-// }
+function middleware1(req,res,next)
+{
+    numberOfRequests++;
+    console.log('Number of requests to the server is '+numberOfRequests);
+    console.log('Inside middleware');
+    next();         // this will call the next middleware in sequence (if any) or the specified handler in 
+                    // the http request
+}
 
-app.use(bodyparser.json());
+app.use(middleware1);
+app.use(bodyParser.json());
 
 function calculateSum(n)
 {
@@ -29,53 +36,192 @@ function calculateSum(n)
     return sum;
 }
 
-// HANDLERS -->
+function calculateProduct(n)
+{
+    let answer = 1;
+    for(let i = 1;i <= n;i++)
+    {
+        answer*= i;
+    }
+    return answer;
+}
+
+// HANDLERS FOR ALL THE HTTP REQUEST METHODS
 
 function handleFirstRequest(req,res)
 {
-    // res.send(calculateSum(100));    // this will throw an error because if we send any integer through res.send(number), nodejs takes it as a "STATUS CODE RESPONSE"
-    // res.send(calculateSum(100).toString(10));
+    // the browser needs to send the input to this counter function, there are 3 ways to do this -> query params,headers,body
+    let number = req.query.counter;
+    console.log(calculateSum(req.query.counter1));
+    console.log(calculateSum(req.query.counter2));
+    let calculatedSum = calculateSum(number);
+    // res.send(calculatedSum); this will throw an error as Invalid status code:5050 because the res.send() takes
+    // any number as a status code
+    let num1 = calculatedSum.toString(10);
+    res.send(num1);
+} 
 
-    let input1 = req.query.value;       // if we want sum of user given value, we can pass it in the query parameter while "sending HTTP REQUEST"
-    res.send('The sum is '+calculateSum(input1));
 
-    // there're other ways to send data to the BACKEND SERVER (data on which complex tasks can be performed) from the REQUEST SIDE
-    // using 'HEADERS' , 'BODY'
-    // but we can only send query params from the browser, rest can be sent with tools such as thunder client or postman
+function createUser(req,res)
+{
+    let num1 = req.headers.counter;
+    let num2 = req.headers.counter1;
+
+    console.log(req.headers);
+
+    console.log('Sum1 '+calculateSum(num1));
+    console.log('Sum2 '+calculateSum(num2));
+    console.log(req.query);
+    console.log(req.query.name);
+    console.log(req.route);
+    console.log(req.route.path);
+    res.send('User is created');
+}
+
+function handleUser(req,res)
+{
+    let name = req.query.name;
+    console.log(req.params.user);
+    res.send('username : '+name);   
 }
 
 function handleSecondRequest(req,res)
 {
-    let num1 = req.headers.val;
-    let num2 = req.query.counter;
-    // we can't send headers from the http requests send by browsers, only from tools like thunder-client or postman
-    console.log('sum is '+calculateSum(num1));
-    res.send('calculated sum is '+calculateSum(num2));
-}
-
-function handleThirdrequest(req,res)
-{   
     console.log(req.body);
-    console.log(req.query);
-    console.log(req.headers);
-    let num1 = req.headers.header1;
-    res.send('the sum here is '+calculateSum(num1));
+    let num1 = req.body.value1;
+    let num2 = req.body.value2;
+
+
+    if(num1 < 1000000)
+    {
+        let calculatedSum = calculateSum(num1);
+        let calculatedProduct = calculateProduct(num2);
+
+        let answerObj = {
+            sum : calculatedSum,
+            product : calculatedProduct
+        }
+
+        if(calculatedSum > 100000000)
+        {
+            answerObj['veryLargeSum'] = true;
+        }
+        else
+        {
+            answerObj['veryLargeSum'] = false;
+        }
+
+        if(calculatedSum > 10000 && !answerObj['veryLargeSum'])
+        {
+            answerObj['LargeSum'] = true;
+        }
+        else
+        {
+            answerObj['LargeSum'] = false;
+        }
+        res.status(200).json(answerObj);
+    }
+    else 
+    {
+        res.status(401).send('Very big number for calculateSum');
+    }
 }
 
-function createUser(req,res)
+function getApp(req,res)
 {
-    res.send('User is created');
+//     res.send(`<html>
+//     <head>
+//         <title>todoApp</title> 
+//         <h1>todoList</h1>
+//     </head>
+//     <body>
+//         <b>
+//             <li>Coding</li>
+//             <li>Gym</li>
+//             <li>Running</li>
+//             <li>Reading</li>
+//         </b>
+//     </body>
+// </html>`); 
+
+    // another way to send html is by directly sending the html file to the browser(or frontend)
+    res.sendFile(__dirname+"/index.html");
 }
 
-// REQEUST METHODS -->
+function fetchApi(req,res)
+{
+    let object1 = {
+        "data": [{
+          "type": "articles",
+          "id": "1",
+          "attributes": {
+            "title": "JSON:API paints my bikeshed!",
+            "body": "The shortest article. Ever.",
+            "created": "2015-05-22T14:56:29.000Z",
+            "updated": "2015-05-22T14:56:28.000Z"
+          },
+          "relationships": {
+            "author": {
+              "data": {"id": "42", "type": "people"}
+            }
+          }
+        }],
+        "included": [
+          {
+            "type": "people",
+            "id": "42",
+            "attributes": {
+              "name": "John",
+              "age": 80,
+              "gender": "male"
+            }
+          }
+        ]
+      };
 
-// the browser (mostly) is capable of only sending get requests to the server
-app.get('/',handleFirstRequest);    
-app.get('/handleSum',handleSecondRequest);
-app.post('/handleSum1',handleThirdrequest);
-// app.get('/:username',createUser);           // this is a catch all route, it catches all requests of /name, /name2 type
+      let object2 = [
+        {
+            color: "red",
+            value: "#f00"
+        },
+        {
+            color: "green",
+            value: "#0f0"
+        },
+        {
+            color: "blue",
+            value: "#00f"
+        },
+        {
+            color: "cyan",
+            value: "#0ff"
+        },
+        {
+            color: "magenta",
+            value: "#f0f"
+        },
+        {
+            color: "yellow",
+            value: "#ff0"
+        },
+        {
+            color: "black",
+            value: "#000"
+        }
+    ];
 
-// SERVER LISTEN METHODS
+    res.send(object1);
+    // res.json(object2);
+}
+
+// REQUEST METHODS
+
+app.get('/handleSum',handleFirstRequest);
+app.post('/createUser',createUser);
+// app.get('/:user',handleUser); 
+app.post('/handleSum1',handleSecondRequest);
+app.get('/todoApp',getApp);
+app.get('/getApi',fetchApi);
 
 function started()
 {
@@ -83,10 +229,3 @@ function started()
 }
 
 app.listen(port,started);
-
-// function started1()
-// {
-//     console.log(`Example app started listening on port ${port1}`);
-// }
-
-// app.listen(port1,started1);
---
+++ secondProcess.js
@@ -0,0 +1,15 @@
+
+let sendObj = {
+    method : "POST"
+};
+
+function callbackFn(result)
+{
+    console.log(result);
+}
+
+// just like we send http requests(as get requests) from the browser, and POST requests using thunder-client or postman,
+// we're able to send requests to an (already) running nodejs prcoess(/handleSum here) from this another nodejs process (secondProcess.js)
+
+// we have 1. URL/route     2. request method sent inside an object    3. handler (as the callbackFn here)
+fetch("http://localhost:3000/handleSum",sendObj).then(callbackFn);
